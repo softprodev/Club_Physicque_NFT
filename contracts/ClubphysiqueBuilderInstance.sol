475ed824996dbe23ec4d7a26bd1017f127c1aa33
softprodev <softprodev15@gmail.com>
2022-03-08 18:44:51 -0500

    Add/RemoveTokenFromOwner, Interface for IERCMetadata

--- contracts/ClubphysiqueBuilderInstance.sol
+++ contracts/ClubphysiqueBuilderInstance.sol
@@ -706,4 +706,77 @@ contract ERC721Enumerable is Context, ERC165, ERC721, IERC721Enumerable {
        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
        _ownedTokens[to].push(tokenId);
    }
+ /**
+    * @dev Private function to add a token to this extension's token tracking data structures.
+    * @param tokenId uint256 ID of the token to be added to the tokens list
+    */
+   function _addTokenToAllTokensEnumeration(uint256 tokenId) private {
+       _allTokensIndex[tokenId] = _allTokens.length;
+       _allTokens.push(tokenId);
+   }
+
+   /**
+    * @dev Private function to remove a token from this extension's ownership-tracking data structures. Note that
+    * while the token is not assigned a new owner, the `_ownedTokensIndex` mapping is _not_ updated: this allows for
+    * gas optimizations e.g. when performing a transfer operation (avoiding double writes).
+    * This has O(1) time complexity, but alters the order of the _ownedTokens array.
+    * @param from address representing the previous owner of the given token ID
+    * @param tokenId uint256 ID of the token to be removed from the tokens list of the given address
+    */
+   function _removeTokenFromOwnerEnumeration(address from, uint256 tokenId) private {
+       // To prevent a gap in from's tokens array, we store the last token in the index of the token to delete, and
+       // then delete the last slot (swap and pop).
+
+       uint256 lastTokenIndex = _ownedTokens[from].length.sub(1);
+       uint256 tokenIndex = _ownedTokensIndex[tokenId];
+
+       // When the token to delete is the last token, the swap operation is unnecessary
+       if (tokenIndex != lastTokenIndex) {
+           uint256 lastTokenId = _ownedTokens[from][lastTokenIndex];
+
+           _ownedTokens[from][tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
+           _ownedTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
+       }
+
+       // This also deletes the contents at the last position of the array
+       _ownedTokens[from].length--;
+
+       // Note that _ownedTokensIndex[tokenId] hasn't been cleared: it still points to the old slot (now occupied by
+       // lastTokenId, or just over the end of the array if the token was the last one).
+   }
+
+   /**
+    * @dev Private function to remove a token from this extension's token tracking data structures.
+    * This has O(1) time complexity, but alters the order of the _allTokens array.
+    * @param tokenId uint256 ID of the token to be removed from the tokens list
+    */
+   function _removeTokenFromAllTokensEnumeration(uint256 tokenId) private {
+       // To prevent a gap in the tokens array, we store the last token in the index of the token to delete, and
+       // then delete the last slot (swap and pop).
+
+       uint256 lastTokenIndex = _allTokens.length.sub(1);
+       uint256 tokenIndex = _allTokensIndex[tokenId];
+
+       // When the token to delete is the last token, the swap operation is unnecessary. However, since this occurs so
+       // rarely (when the last minted token is burnt) that we still do the swap here to avoid the gas cost of adding
+       // an 'if' statement (like in _removeTokenFromOwnerEnumeration)
+       uint256 lastTokenId = _allTokens[lastTokenIndex];
+
+       _allTokens[tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
+       _allTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
+
+       // This also deletes the contents at the last position of the array
+       _allTokens.length--;
+       _allTokensIndex[tokenId] = 0;
+   }
+}
+
+/**
+* @title ERC-721 Non-Fungible Token Standard, optional metadata extension
+* @dev See https://eips.ethereum.org/EIPS/eip-721
+*/
+contract IERC721Metadata is IERC721 {
+   function name() external view returns (string memory);
+   function symbol() external view returns (string memory);
+   function tokenURI(uint256 tokenId) external view returns (string memory);
 }
