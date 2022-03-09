d7e83b8efeb281a3f442706671385a50f85cbc1d
softprodev <softprodev15@gmail.com>
2022-03-09 18:30:42 -0500

    ERC721Metadata

--- contracts/ClubphysiqueBuilderInstance.sol
+++ contracts/ClubphysiqueBuilderInstance.sol
@@ -780,3 +780,66 @@ contract IERC721Metadata is IERC721 {
    function symbol() external view returns (string memory);
    function tokenURI(uint256 tokenId) external view returns (string memory);
 }
+
+
+contract ERC721Metadata is Context, ERC165, ERC721, IERC721Metadata {
+   // Token name
+   string private _name;
+
+   // Token symbol
+   string private _symbol;
+
+   // Optional mapping for token URIs
+   mapping(uint256 => string) private _tokenURIs;
+  
+   
+   //Optional mapping for IPFS link to canonical image file
+   mapping(uint256 => string) private _tokenIPFSHashes;
+
+   /*
+    *     bytes4(keccak256('name()')) == 0x06fdde03
+    *     bytes4(keccak256('symbol()')) == 0x95d89b41
+    *     bytes4(keccak256('tokenURI(uint256)')) == 0xc87b56dd
+    *
+    *     => 0x06fdde03 ^ 0x95d89b41 ^ 0xc87b56dd == 0x5b5e139f
+    */
+   bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f; // need to change
+
+   /**
+    * @dev Constructor function
+    */
+   constructor (string memory name, string memory symbol) public {
+       _name = name;
+       _symbol = symbol;
+
+       // register the supported interfaces to conform to ERC721 via ERC165
+       _registerInterface(_INTERFACE_ID_ERC721_METADATA);
+   }
+
+   /**
+    * @dev Gets the token name.
+    * @return string representing the token name
+    */
+   function name() external view returns (string memory) {
+       return _name;
+   }
+
+   /**
+    * @dev Gets the token symbol.
+    * @return string representing the token symbol
+    */
+   function symbol() external view returns (string memory) {
+       return _symbol;
+   }
+
+   /**
+    * @dev Returns an URI for a given token ID.
+    * Throws if the token ID does not exist. May return an empty string.
+    * @param tokenId uint256 ID of the token to query
+    */
+   function tokenURI(uint256 tokenId) external view returns (string memory) {
+       require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+       return _tokenURIs[tokenId];
+   }
+   
+}
