f01bbd37638560ee7a543cfadecc4d75ac8442a7
softprodev <softprodev15@gmail.com>
2022-03-15 18:52:45 -0400

    Address and counter management

--- contracts/ClubphysiqueBuilderInstance.sol
+++ contracts/ClubphysiqueBuilderInstance.sol
@@ -1199,3 +1199,104 @@ library SafeMath {
        return a % b;
    }
 }
+
+/**
+* @dev Collection of functions related to the address type
+*/
+library Address {
+   /**
+    * @dev Returns true if `account` is a contract.
+    *
+    * This test is non-exhaustive, and there may be false-negatives: during the
+    * execution of a contract's constructor, its address will be reported as
+    * not containing a contract.
+    *
+    * IMPORTANT: It is unsafe to assume that an address for which this
+    * function returns false is an externally-owned account (EOA) and not a
+    * contract.
+    */
+   function isContract(address account) internal view returns (bool) {
+       // This method relies in extcodesize, which returns 0 for contracts in
+       // construction, since the code is only stored at the end of the
+       // constructor execution.
+
+       // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
+       // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
+       // for accounts without code, i.e. `keccak256('')`
+       bytes32 codehash;
+       bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470; // need to change
+       // solhint-disable-next-line no-inline-assembly
+       assembly { codehash := extcodehash(account) }
+       return (codehash != 0x0 && codehash != accountHash);
+   }
+
+   /**
+    * @dev Converts an `address` into `address payable`. Note that this is
+    * simply a type cast: the actual underlying value is not changed.
+    *
+    * NOTE: This is a feature of the next version of OpenZeppelin Contracts.
+    * @dev Get it via `npm install @openzeppelin/contracts@next`.
+    */
+   function toPayable(address account) internal pure returns (address payable) {
+       return address(uint160(account));
+   }
+
+   /**
+    * @dev Replacement for Solidity's `transfer`: sends `amount` wei to
+    * `recipient`, forwarding all available gas and reverting on errors.
+    *
+    * https://eips.ethereum.org/EIPS/eip-1884[EIP1884] increases the gas cost
+    * of certain opcodes, possibly making contracts go over the 2300 gas limit
+    * imposed by `transfer`, making them unable to receive funds via
+    * `transfer`. {sendValue} removes this limitation.
+    *
+    * https://diligence.consensys.net/posts/2019/09/stop-using-soliditys-transfer-now/[Learn more].
+    *
+    * IMPORTANT: because control is transferred to `recipient`, care must be
+    * taken to not create reentrancy vulnerabilities. Consider using
+    * {ReentrancyGuard} or the
+    * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
+    */
+   function sendValue(address payable recipient, uint256 amount) internal {
+       require(address(this).balance >= amount, "Address: insufficient balance");
+
+       // solhint-disable-next-line avoid-call-value
+       (bool success, ) = recipient.call.value(amount)("");
+       require(success, "Address: unable to send value, recipient may have reverted");
+   }
+}
+
+/**
+* @title Counters
+* @author Matt Condon (@shrugs)
+* @dev Provides counters that can only be incremented or decremented by one. This can be used e.g. to track the number
+* of elements in a mapping, issuing ERC721 ids, or counting request ids.
+*
+* Include with `using Counters for Counters.Counter;`
+* Since it is not possible to overflow a 256 bit integer with increments of one, `increment` can skip the {SafeMath}
+* overflow check, thereby saving gas. This does assume however correct usage, in that the underlying `_value` is never
+* directly accessed.
+*/
+library Counters {
+   using SafeMath for uint256;
+
+   struct Counter {
+       // This variable should never be directly accessed by users of the library: interactions must be restricted to
+       // the library's function. As of Solidity v0.5.2, this cannot be enforced, though there is a proposal to add
+       // this feature: see https://github.com/ethereum/solidity/issues/4637
+       uint256 _value; // default: 0
+   }
+
+   function current(Counter storage counter) internal view returns (uint256) {
+       return counter._value;
+   }
+
+   function increment(Counter storage counter) internal {
+       // The {SafeMath} overflow check can be skipped here, see the comment at the top
+       counter._value += 1;
+   }
+
+   function decrement(Counter storage counter) internal {
+       counter._value = counter._value.sub(1);
+   }
+}
